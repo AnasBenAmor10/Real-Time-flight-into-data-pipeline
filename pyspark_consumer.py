d43d438681c9af6bb0c0fc20c4fbd544fd89b9f9
AnasBenAmor10 <Anas.benamor@supcom.tn>
2024-01-31 19:11:56 +0100

    create a elasticsearch client  and create a mapping

--- pyspark_consumer.py
+++ pyspark_consumer.py
@@ -1,13 +1,52 @@
 #import the necessary library
 from pyspark.sql import SparkSession
 from pyspark.conf import SparkConf
+from elasticsearch import Elasticsearch
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-from pyspark.sql.functions import from_json , first, col , when , length
+from pyspark.sql.functions import from_json , col , when , length
 
 
+# Create an Elasticsearch client
+es = Elasticsearch([{'host': 'elasticsearch', 'port': 9200, 'scheme': 'http'}])
+
+# Define the index name
+index_name = "flight"
+
+# Check if the Elasticsearch index exists
+if es.indices.exists(index=index_name):
+    # Delete the index
+    es.indices.delete(index=index_name)
+    print(f"Index '{index_name}' has been deleted.")
+else:
+    print(f"Index '{index_name}' does not exist.")
 
 
+# Define the mapping for Elasticsearch index
+mapping = {
+    "mappings": {
+        "properties": {
+         "hex": { "type": "integer" },
+         "reg_number":{"type": "keyword"},
+         "flag":{"type":"keyword"},
+         "position": {
+            "type": "geo_point"
+         },
+         "speed":{"type":"integer"},
+         "v_speed":{"type":"integer"},
+         "flight_number":{"type":"integer"},
+         "flight_iata":{"type":"keyword"},
+         "dep_iata":{"type":"keyword"},
+         "arr_iata":{"type":"keyword"},
+         "airline_iata":{"type":"keyword"},
+         "aircraft_icao": { "type": "keyword" },
+         "status": { "type": "keyword" },
+              }
+    }
+    }
+
+
+#The structure of the data  received from a Kafka topic 
 schema = StructType([
     # Define the schema for kafka message
     StructField("hex", IntegerType(), True),
@@ -20,6 +59,7 @@ schema = StructType([
         StructField("dir", DoubleType(), True)
     ]), True),
     StructField("speed", IntegerType(), True),
+    StructField("v_speed", IntegerType(), True),
     StructField("flight_number", IntegerType(), True),
     StructField("flight_icao", StringType(), True),
     StructField("flight_iata", StringType(), True),
@@ -65,8 +105,6 @@ json_df = kafka_data.selectExpr("CAST(value AS STRING)") \
 
 # Print the schema of the DataFrame
 json_df.printSchema()
-
-
 
 # Add the filter condition to final_result
 final_result_filtered = json_df \
