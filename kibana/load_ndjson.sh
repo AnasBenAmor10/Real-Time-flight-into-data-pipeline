d2ab7f1d7b2900dbcefb9afc65f039e7a0d856bc
AnasBenAmor10 <Anas.benamor@supcom.tn>
2024-04-08 00:51:06 +0200

    docker compose kibana and dockerfile

--- kibana/load_ndjson.sh
+++ kibana/load_ndjson.sh
@@ -1,25 +1,12 @@
-#!/bin/bash
 
-# Kibana URL
-KIBANA_URL='http://localhost:5601'
-KIBANA_IMPORT_API="$KIBANA_URL/api/saved_objects/_import"
-NDJSON_FILE_PATH='../kibana/export.ndjson'
-
-# Wait for Kibana to be ready
-until curl -s "$KIBANA_URL" -o /dev/null; do
-    echo "Waiting for Kibana..."
+until curl -s http://kibana:5601/login -o /dev/null; do
+    echo Waiting for Kibana...
     sleep 10
 done
 echo "Kibana is ready."
 
-# Sleep a little longer to ensure all services are up
+# The import service may not be ready, sleep 10s more.
 sleep 10
+curl -X POST kibana:5601/api/saved_objects/_import -H "kbn-xsrf: true" --form file=@/tmp/load/export.ndjson
 
-# Import the .ndjson file
-response=$(curl -s -w "%{http_code}" -o /dev/null -X POST "$KIBANA_IMPORT_API" -H "kbn-xsrf: true" --form file=@"$NDJSON_FILE_PATH")
-
-if [ "$response" -eq 200 ]; then
-    echo "Export.ndjson imported successfully."
-else
-    echo "Failed to import export.ndjson. HTTP response code: $response"
-fi
+echo 'Done ! Kibana is ready.'
