29ec767877719ee03a6a9cb084a0be501277cc61
AnasBenAmor10 <Anas.benamor@supcom.tn>
2024-05-05 12:16:46 +0200

    run.sh

--- run.bat
+++ run.bat
@@ -1,9 +1,6 @@
-@REM  Copy pyspark_consumer.py to the Spark container
-
-docker cp ./pyspark_consumer.py real-time-flight-into-data-pipeline-spark-master-1:/opt/bitnami/spark/pyspark_consumer.py
 @REM  Run create_index.py
 
-python create_index_elastic.py
+python3 create_index_elastic.py
 @REM Submit the Spark job
 
 docker-compose exec spark-master spark-submit --class consumer --total-executor-cores 4 --executor-memory 2g --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.4,org.elasticsearch:elasticsearch-spark-30_2.12:8.8.2,commons-httpclient:commons-httpclient:3.1 pyspark_consumer.py >out.txt 
--- run.sh
+++ run.sh
@@ -4,4 +4,7 @@
 
 python3 create_index_elastic.py
 
+#Command run
+docker-compose exec spark-master spark-submit --class consumer --total-executor-cores 4 --executor-memory 2g --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.4,org.elasticsearch:elasticsearch-spark-30_2.12:8.8.2,commons-httpclient:commons-httpclient:3.1 pyspark_consumer.py
+
 echo "Done"
