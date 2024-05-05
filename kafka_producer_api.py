7efac8448e9369247b1b861a847901c1bd99d6af
AnasBenAmor10 <Anas.benamor@supcom.tn>
2024-05-05 12:18:30 +0200

    fix kafka

--- kafka_producer_api.py
+++ kafka_producer_api.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 import os
 
 
-# bootstrap_servers = 'localhost:9093'
-bootstrap_servers = 'localhost:9092'
+bootstrap_servers = 'localhost:9093'
+#bootstrap_servers = 'localhost:9092'
 
 # Create a Kafka producer
 producer = KafkaProducer(bootstrap_servers=bootstrap_servers, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
