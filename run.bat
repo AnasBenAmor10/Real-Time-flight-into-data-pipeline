00cf80b20d6ae3e56c1869811c3ca58a42a61905
AnasBenAmor10 <Anas.benamor@supcom.tn>
2024-05-29 20:14:19 +0200

    update the workflow

--- run.bat
+++ run.bat
@@ -3,4 +3,4 @@
 python3 create_index_elastic.py
 @REM Submit the Spark job
 
-docker-compose exec spark-master spark-submit --class consumer --total-executor-cores 4 --executor-memory 2g --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.4,org.elasticsearch:elasticsearch-spark-30_2.12:8.8.2,commons-httpclient:commons-httpclient:3.1 pyspark_consumer.py >out.txt 
+docker-compose exec spark-master spark-submit --class consumer --total-executor-cores 4 --executor-memory 2g --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.4,org.elasticsearch:elasticsearch-spark-30_2.12:8.8.2,commons-httpclient:commons-httpclient:3.1 spark_consumer.py >out.txt 
